c0628cf5a1be46fc5cd1f9da06d81a6f7f59cac7
Antus8 <antus.longo@gmail.com>
2021-05-02 16:42:45 +0200

    Removing server folder

--- SERVER+DB/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_name = "yourownfitnessdb";
-$mysql_username = "root";
-$mysql_password = "";
-$server_name = "localhost:3306";
-
-$conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
-?>
